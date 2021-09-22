4c99411bc0f711097cb69296fc11c1e4380622fd
Alex <alexmunoz91@gmail.com>
2021-09-22 19:57:45 +0200

    Add protocolTypes

---
+++ backend/src/utils/protocolTypes.ts
@@ -0,0 +1,8 @@
+export default {
+  closest_enemies: 'closest-enemies',
+  furthest_enemies: 'furthest-enemies',
+  assist_allies: 'assist-allies',
+  avoid_crossfire: 'avoid-crossfire',
+  prioritize_mech: 'prioritize-mech',
+  avoid_mech: 'avoid-mech'
+};
