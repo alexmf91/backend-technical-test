b60836dc6733a25231a7831860a6ba0fa347cb07
Alex <alexmunoz91@gmail.com>
2021-09-22 20:04:58 +0200

    Add post method

--- backend/src/router/droidRoutes.ts
+++ backend/src/router/droidRoutes.ts
@@ -1,8 +1,10 @@
 const { Router } = require('express');
+const { droidController } = require('../controllers/droidController.ts');
 
 const router = new Router();
 
 router
   .route('/radar')
+  .post(droidController);
 
 module.exports = router;
